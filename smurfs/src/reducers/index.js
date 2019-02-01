fa3bcda11c9e6c9659b88c853cde65719e811e89
KROHNICK <business.kanekperez@gmail.com>
2019-02-01 09:21:05 -0800

    Completed actions

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,74 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCHING,
+  SUCCESS,
+  FAILURE,
+  CREATING,
+  CREATED,
+  UPDATING,
+  UPDATED,
+  DELETING,
+  DELETED
+} from "../actions";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload
+      };
+    case FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      };
+    case CREATING:
+      return {
+        ...state,
+        addingSmurf: true
+      };
+    case CREATED:
+      return {
+        ...state,
+        smurfs: action.payload
+      };
+    case UPDATING:
+      return {
+        ...state,
+        updatingSmurf: true
+      };
+    case UPDATED:
+      return {
+        ...state,
+        smurfs: action.payload
+      };
+    case DELETING:
+      return {
+        ...state,
+        deletingSmurf: true
+      };
+    case DELETED:
+      return {
+        ...state,
+        smurfs: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default smurfReducer;
